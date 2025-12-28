f3de377e1b2a57b7a1f952dd87d166a764cf1751
unknown <narendrasinghgangwar>
2025-12-28 18:36:37 +0530

    loadlancer and nic loaded updated

--- Infra24122025/Modules/azurerm_virtual_machine/main.tf
+++ Infra24122025/Modules/azurerm_virtual_machine/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_network_interface" "nicb" {
   resource_group_name = each.value.rg_name
 
   ip_configuration {
-    name                          = "ip_configuration1"
+    name                          = "internal"
     subnet_id                     = data.azurerm_subnet.subnet_id[each.key].id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = data.azurerm_public_ip.public_ip_address_id[each.key].id
